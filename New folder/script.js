8a8206e022aca5dfd3ea69060f1dd4759e8312ca
Rakshan28-lab <rakshananandakrishnan88@gmail.com>
2026-01-08 14:35:54 +0530

    index and  js

--- New folder/script.js
+++ New folder/script.js
@@ -1,24 +1,23 @@
 (function() {
-    // 1. Initialize with your Public Key (from EmailJS dashboard)
     emailjs.init("D5YnwovUGYAzA1FHt");
 })();
 
 window.onload = function() {
+    // This must match the ID in your HTML tag
     const form = document.getElementById('contact-form');
+    const btn = document.getElementById('submit-btn');
     
     form.addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevents page reload
+        event.preventDefault(); 
         
-        const btn = document.getElementById('submit-btn');
         btn.innerText = 'Sending...';
 
-        // 2. Send the form data
-        // "service_id" and "template_id" come from your EmailJS account
-        emailjs.sendForm('service_qg3dmh5', 'YOUR_TEMPLATE_ID', this)
+       
+        emailjs.sendForm('service_qg3dmh5', 'template_vg17stk', this)
             .then(function() {
                 alert('Sent Successfully!');
                 btn.innerText = 'Send Message';
-                form.reset(); // Clears the form
+                form.reset(); 
             }, function(error) {
                 alert('Failed to send... ' + JSON.stringify(error));
                 btn.innerText = 'Send Message';
